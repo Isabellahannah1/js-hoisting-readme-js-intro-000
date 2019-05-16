95816c7dc7718c3e8b8ae7301595d3648d7bdc5c
Isabella Hannah <isabellahannah735@gmail.com>
2019-05-16 15:57:46 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -1,12 +1,39 @@
-function callMe(){
-  return "maybe";
+function callMe() {
+    var lyric = "maybe";
+    console.log("I just met you...");
+    console.log("and this is crazy..");
+    console.log("but here's my number..");
+    console.log("so call me");
+    return lyric;
+}	
+
+<<<<<<< HEAD
+
+function crazy() { 
+  var thisIsCrazy = function (){
+    console.log("hey!!!")
+  }
+thisIsCrazy();
 }
 
-function sayMyName(){{
-  console.log("Kristin");
-}
 
-function thisIsCrazy(){
-  console.log("hey!!!");
+function sayMyName() {
+  
+  var name = "Cricky";
+  function sayMy() {
+  var name = "Kristin";
+  console.log(name);
+  }
+   sayMy();
 }
+=======
+  function thisIsCrazy(){
+    console.log("hey!!!")
+  }
+thisIsCrazy();
+
+function sayMyName(){
+  console.log("Cricky")
 }
+  sayMyName();
+>>>>>>> 1c9386962316908fe5d1971a1d0743d014ceef9f
