acbbf30c302f50cd839343322c9385cb259d15fa
Isabella Hannah <isabellahannah735@gmail.com>
2019-05-16 15:56:04 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -2,14 +2,11 @@ function callMe(){
   return "maybe";
 }
 
-function sayMyName(){
+function sayMyName(){{
   console.log("Kristin");
 }
-
-sayMy();
 
 function thisIsCrazy(){
   console.log("hey!!!");
 }
-
-thisIsCrazy();
+}
