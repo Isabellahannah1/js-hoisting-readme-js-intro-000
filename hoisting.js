4c061a1a939e021e305d827da5a25741f50d0ae1
Isabella Hannah <isabellahannah735@gmail.com>
2019-05-16 16:36:59 +0000

    Automatically backed up by Learn

---
+++ hoisting.js
@@ -0,0 +1,26 @@
+function callMe() {
+    var lyric = "maybe";
+    console.log("I just met you...");
+    console.log("and this is crazy..");
+    console.log("but here's my number..");
+    console.log("so call me");
+    return lyric;
+}	
+
+
+function crazy() { 
+  
+  var thisIsCrazy = function (){
+    console.log("hey!!!")
+  }
+thisIsCrazy();
+}
+
+var name = "Cricky";
+
+function sayMyName() {
+  var name = "Kristin";
+  console.log(name);
+  }
+  
+   sayMyName();
